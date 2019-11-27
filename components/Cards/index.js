689d3f952d7e4760dbae06a88c7d29069fa321bb
Mauricio Degregori <degregori2@gmail.com>
2019-11-26 18:13:49 -0600

    finished for extra practice

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,49 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const articleCards = document.querySelector('.cards-container');
+fetchData = axios
+.get('https://lambda-times-backend.herokuapp.com/articles')
+.then( response => {
+    console.log('last get', response.data.articles);
+    const allCards = Object.entries(response.data.articles);
+    allCards.map( topic => {
+        topic[1].map( e => {
+            articleCards.appendChild(Card(e));
+        });
+    });
+})
+.catch(err => {
+    console.log(err);
+})
+
+const Card = e => {
+    const card = document.createElement(`div`);
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div')
+    const img = document.createElement('img');
+    const authorsName = document.createElement('span')
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    headline.textContent = e.headline;
+    authorsName.textContent = e.authorName;
+    img.src = e.authorPhoto;
+
+    // append to one another 
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(img);
+    author.appendChild(authorsName)
+
+    return card;
+
+
+}
+
